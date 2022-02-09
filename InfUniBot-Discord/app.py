7902f07838fb4521a5b54e87dc732489db330280
Vincenzo Cozzi <vincenzo.cozzi256@gmail.com>
2022-02-09 15:45:25 +0100

    Aggiunti i comandi con lo slash, aggiunti argomenti al comando ;corsi

--- InfUniBot-Discord/app.py
+++ InfUniBot-Discord/app.py
@@ -9,12 +9,11 @@ from discord.utils import get
 from config import *
 from strings import *
 
-bot = commands.Bot(command_prefix='.', help_command=None)
+bot = commands.Bot(command_prefix=';', help_command=None)
 slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
-    
     print(f'{bot.user.name} has connected to Discord!')
 
 @bot.command(name='help')
@@ -26,19 +25,44 @@ async def help_query(ctx):
     await ctx.send(embed=embed) 
 
 @bot.command(name='corsi')
-async def courses_query(ctx):
+async def courses_query(ctx, anno:str="", semestre:str=""):
 
     embed = discord.Embed()
-    embed.title = "Lista completa di tutti i corsi"
-    embed.description = courses_string
-    await ctx.send(embed=embed)     
+    embed.title = "Lista dei link ai corsi"
+
+    if anno == "1":
+        if anno == "1" and semestre == "1":
+            embed.description = first_year_first_semester_string
+            embed.title = "Lista dei link ai corsi "
+            await ctx.send(embed=embed)
+        elif  anno == "1" and semestre == "2":
+            embed.description = first_year_second_semester_string
+            await ctx.send(embed=embed)
+        else:
+            embed.description = first_year_string
+            await ctx.send(embed=embed)
+
+    if anno == "2":
+        if anno == "2" and semestre == "1":
+            embed.description = second_year_first_semester_string
+            await ctx.send(embed=embed)
+        elif  anno == "2" and semestre == "2":
+            embed.description = second_year_second_semester_string
+            await ctx.send(embed=embed)
+        else:
+            embed.description = second_year_string
+            await ctx.send(embed=embed)
+    
+    if anno == "":
+        embed.description = "Selezionare l'anno e/o il semestre ( ;corsi <anno> <semestre> ) \n Ricordati di inserire lo spazio tra <anno> e <semestre>"
+        await ctx.send(embed=embed)
 
 @bot.command(name='s1')
 async def first_semester_query(ctx):
 
     embed = discord.Embed()
     embed.title = "Lista dei corsi del primo semestre"
-    embed.description = first_semester_string
+    embed.description = first_year_first_semester_string
     await ctx.send(embed=embed) 
 
 @bot.command(name='s2')
@@ -46,7 +70,7 @@ async def second_semester_query(ctx):
 
     embed = discord.Embed()
     embed.title = "Lista dei corsi del secondo semestre"
-    embed.description = second_semester_string
+    embed.description = first_year_first_semester_string
     await ctx.send(embed=embed)
     
 @bot.command(name='social')
@@ -77,17 +101,17 @@ async def unisa_query(ctx):
 @bot.command(name='info')
 async def info_query(ctx):
 
-    lux = get(ctx.guild.roles, name="Lux")
+    pommodoro = get(ctx.guild.roles, name="Pommodoro")
     bruschette = get(ctx.guild.roles, name="Bruschette")
     embed = discord.Embed()
     embed.title = "Info server e bot"
-    embed.description = f"""\nPer info riguardanti il server discord, contattare i membri con tag {bruschette.mention} o {lux.mention}\n 
-                            \nPer info e/o segnalazione di bug riguardanti il bot, contattare <@682322414772027443>\n"""
+    embed.description = f"""\nPer info riguardanti il server discord, contattare i membri con tag {bruschette.mention} o {pommodoro.mention}\n 
+                            \nPer info e/o segnalazione di bug riguardanti il bot, contattare <@682322414772027443>\n""" + "\n \U00002615 " + ' ' + " [Bello un caffè a piacere](" + URLS['donation'] +")\n"
     await ctx.send(embed=embed) 
 
 @slash.slash(
     name="corsi",
-    description="Link per i corsi",
+    description="Lista completa dei link ai canali telegram di tutti i corsi",
     guild_ids=[757877506270429184],
     options=[
         create_option(
@@ -124,10 +148,10 @@ async def info_query(ctx):
         )
     ]
 )
-async def _hello(ctx:SlashContext, anno:str, semestre:str=""):
+async def _corsi(ctx:SlashContext, anno:str, semestre:str=""):
 
     embed = discord.Embed()
-    embed.title = "Link dei Corsi"
+    embed.title = "Lista dei link ai corsi"
 
     if anno == "a1":
         if anno == "a1" and semestre == "s1":
@@ -150,6 +174,72 @@ async def _hello(ctx:SlashContext, anno:str, semestre:str=""):
         else:
             embed.description = second_year_string
             await ctx.send(embed=embed)
+
+@slash.slash(
+    name="social",
+    description="Link ai social della nostra classe",
+    guild_ids=[757877506270429184]
+)
+async def _social(ctx:SlashContext):
+
+    embed = discord.Embed()
+    embed.title = "Link ai social"
+
+    embed.description = social_string
+    await ctx.send(embed=embed)
+
+@slash.slash(
+    name="unisa",
+    description="Lista link utili ufficiali UNISA",
+    guild_ids=[757877506270429184]
+)
+async def _unisa(ctx:SlashContext):
+
+    embed = discord.Embed()
+    embed.title = "Lista link utili ufficiali UNISA"
+
+    embed.description = unisa_string
+    await ctx.send(embed=embed)
+
+@slash.slash(
+    name="appunti",
+    description="Lista link di appunti vari ed eventuali",
+    guild_ids=[757877506270429184]
+)
+async def _appunti(ctx:SlashContext):
+
+    embed = discord.Embed()
+    embed.title = "Lista link di appunti vari ed eventuali"
+
+    embed.description = note_string
+    await ctx.send(embed=embed)
+
+@slash.slash(
+    name="info",
+    description="Vabbè oh c'è bisogno di spiegare pure questo?",
+    guild_ids=[757877506270429184]
+)
+async def _info(ctx:SlashContext):
+
+    embed = discord.Embed()
+    pommodoro = get(ctx.guild.roles, name="Pommodoro")
+    bruschette = get(ctx.guild.roles, name="Bruschette")
+    embed.title = "Info server e bot"
+    embed.description = f"""\nPer info riguardanti il server discord, contattare i membri con tag {bruschette.mention} o {pommodoro.mention}\n 
+                            \nPer info e/o segnalazione di bug riguardanti il bot, contattare <@682322414772027443>\n""" + "\n \U00002615 " + ' ' + " [Bello un caffè a piacere](" + URLS['donation'] +")\n"
+    await ctx.send(embed=embed) 
+
+@slash.slash(
+    name="help",
+    description="Liste dei comandi di infunibot",
+    guild_ids=[757877506270429184]
+)
+async def _info(ctx:SlashContext):
+
+    embed = discord.Embed()
+    embed.title = "Lista comandi"
+    embed.description = help_string
+    await ctx.send(embed=embed) 
 
 bot.run(TOKEN) 
 
