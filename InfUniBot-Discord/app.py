aa9be97643c4d0fce6d4f49e52484528f3249285
Vincenzo Cozzi <vincenzo.cozzi256@gmail.com>
2022-02-09 01:10:25 +0100

    [infunibot-discord] Aggiunti slash_command, aggiunti link secondo anno

--- InfUniBot-Discord/app.py
+++ InfUniBot-Discord/app.py
@@ -1,13 +1,16 @@
 # bot.py
 import os
 
-from discord.ext import commands
 import discord
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
+from discord.ext import commands
 from discord.utils import get
 from config import *
 from strings import *
 
 bot = commands.Bot(command_prefix='.', help_command=None)
+slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
@@ -28,7 +31,7 @@ async def courses_query(ctx):
     embed = discord.Embed()
     embed.title = "Lista completa di tutti i corsi"
     embed.description = courses_string
-    await ctx.send(embed=embed)      
+    await ctx.send(embed=embed)     
 
 @bot.command(name='s1')
 async def first_semester_query(ctx):
@@ -82,4 +85,72 @@ async def info_query(ctx):
                             \nPer info e/o segnalazione di bug riguardanti il bot, contattare <@682322414772027443>\n"""
     await ctx.send(embed=embed) 
 
-bot.run(TOKEN)
+@slash.slash(
+    name="corsi",
+    description="Link per i corsi",
+    guild_ids=[757877506270429184],
+    options=[
+        create_option(
+            name="anno",
+            description="Seleziona l'anno",
+            required=True,
+            option_type=3,
+            choices=[
+                create_choice( 
+                    name="PrimoAnno",
+                    value="a1"
+                ),
+                create_choice(
+                    name="SecondoAnno",
+                    value="a2"
+                )
+            ]
+        ),
+        create_option(
+            name="semestre",
+            description="Seleziona il semestre",
+            required=False,
+            option_type=3,
+            choices=[
+                create_choice( 
+                    name="PrimoSemestre",
+                    value="s1"
+                ),
+                create_choice(
+                    name="SecondoSemestre",
+                    value="s2"
+                )
+            ]
+        )
+    ]
+)
+async def _hello(ctx:SlashContext, anno:str, semestre:str=""):
+
+    embed = discord.Embed()
+    embed.title = "Link dei Corsi"
+
+    if anno == "a1":
+        if anno == "a1" and semestre == "s1":
+            embed.description = first_year_first_semester_string
+            await ctx.send(embed=embed)
+        elif  anno == "a1" and semestre == "s2":
+            embed.description = first_year_second_semester_string
+            await ctx.send(embed=embed)
+        else:
+            embed.description = first_year_string
+            await ctx.send(embed=embed)
+
+    if anno == "a2":
+        if anno == "a2" and semestre == "s1":
+            embed.description = second_year_first_semester_string
+            await ctx.send(embed=embed)
+        elif  anno == "a2" and semestre == "s2":
+            embed.description = second_year_second_semester_string
+            await ctx.send(embed=embed)
+        else:
+            embed.description = second_year_string
+            await ctx.send(embed=embed)
+
+bot.run(TOKEN) 
+
+
--- InfUniBot-Discord/strings.py
+++ InfUniBot-Discord/strings.py
@@ -10,13 +10,21 @@ help_string = """\n.corsi -> lista completa dei link ai canali telegram di tutti
 
 courses_string = string_array['ADE'] + string_array['PR1'] + string_array['MD'] + string_array['OFA'] + string_array['PSD'] + string_array['MMI'] + string_array['ANAL'] 
 
-first_semester_string = string_array['ADE'] + string_array['PR1'] + string_array['MD'] + string_array['OFA']
+first_year_first_semester_string = string_array['ADE'] + string_array['PR1'] + string_array['MD'] + string_array['OFA']
 
-second_semester_string = string_array['PSD'] + string_array['MMI'] + string_array['ANAL']
+first_year_second_semester_string = string_array['PSD'] + string_array['MMI'] + string_array['ANAL']
+
+first_year_string = first_year_first_semester_string + first_year_second_semester_string
+
+second_year_first_semester_string = string_array['BD'] + string_array['POO'] + string_array['SO']
+
+second_year_second_semester_string = string_array['PA'] + string_array['CPM'] + string_array['RETI'] + string_array['TSW']
+
+second_year_string = second_year_first_semester_string + second_year_second_semester_string
 
 social_string = string_array['whatsapp'] + string_array['telegram'] + string_array['discord']
 
-note_string = string_array['hubmd'] + string_array['nicotera']
+note_string = string_array['hubmd'] + string_array['nicotera'] 
 
 unisa_string = string_array['elearning'] + string_array['unisa'] + string_array['moodle'] + string_array['esse3']
 
